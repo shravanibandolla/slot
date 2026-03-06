e4b8dee2a6cd42571472f5ef5564363c29f47432
Shravani Bandolla <shravani@example.com>
2026-03-06 10:58:39 +0530

    changes done in templates

--- slotbooking/urls.py
+++ slotbooking/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('slots/',include('slots.urls')),
+    path('slots/', include('slots.urls')),
 ]
